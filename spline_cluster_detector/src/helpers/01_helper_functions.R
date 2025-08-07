9dfa7917446b370cdebb7d14c68e4e9acfa4baff
Mullany, Luke C <luke.mullany@jhuapl.edu>
2025-08-07 10:01:37 -0400

    improve regex for CCDD categories

--- spline_cluster_detector/src/helpers/01_helper_functions.R
+++ spline_cluster_detector/src/helpers/01_helper_functions.R
@@ -468,7 +468,7 @@ get_base_vals <- function(use_nssp, profile=NULL) {
       
       setDT(combinedCategories)
       ccdd_cats <- combinedCategories[grepl("^CCDD", combined_category), combined_category]
-      ccdd_cats <- gsub("CCDD ", "", ccdd_cats)
+      ccdd_cats <- gsub("^CCDD ", "", ccdd_cats, perl=T)
       syndromes <- combinedCategories[grepl("^SYNDROME", combined_category), combined_category]
       syndromes <- gsub("SYNDROME ", "", syndromes)
       subsyndromes <- combinedCategories[grepl("^SUBSYNDROME", combined_category), combined_category]
