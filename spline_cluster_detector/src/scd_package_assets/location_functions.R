b47af0fa2f4e55cc09f201179183dc631b1653c9
Mullany, Luke C <luke.mullany@jhuapl.edu>
2025-10-14 08:22:52 -0400

    updated package assets with new speedier distance function

--- spline_cluster_detector/src/scd_package_assets/location_functions.R
+++ spline_cluster_detector/src/scd_package_assets/location_functions.R
@@ -15,7 +15,6 @@
 #' @examples
 #' # example code
 #' zip_distance_matrix("MD")
-#' zip_distance_matrix("TX", "kilometers")
 zip_distance_matrix <- function(
     st,
     unit = c("miles", "kilometers", "meters")) {
@@ -81,7 +80,7 @@ zip_distance_matrix <- function(
 #' @examples
 #' # example code
 #' county_distance_matrix("MD", source = "tigris")
-#' county_distance_matrix("CT", "kilometers", source = "tigris")
+#' county_distance_matrix("CT", "kilometers", source = "rnssp")
 county_distance_matrix <- function(
     st,
     unit = c("miles", "kilometers", "meters"),
@@ -162,13 +161,11 @@ county_distance_matrix <- function(
 #'
 #' Function returns a list of counties and a matrix with the distance between
 #' those counties. leverages a built in dataset (`counties`). Note that the
-#' generation of this matrix can take a few seconds
+#' generation of this matrix can take a few seconds. Note: it is better and
+#' faster to use create_dist_matrix()
 #' @param unit string, one of "miles" (default), "kilometers", or "meters".
 #'   Indicating the desired unit for the distances
 #' @export
-#' @examples
-#' # example code
-#' us_distance_matrix()
 us_distance_matrix <- function(
     unit = c("miles", "kilometers", "meters")) {
   # provide warning re the time it takes to construct this matrix
@@ -218,4 +215,104 @@ us_distance_matrix <- function(
     loc_vec = loc_vec |> as.character(),
     distance_matrix = distance_matrix
   ))
+}
+
+#' Generalized distance list as sparse list
+#'
+#' This function is an alternative to the package functions that create a square
+#' distance matrix of dimension N, with all pairwise distances. In this approach
+#' a list of named vectors is returned, where there is one element in the list
+#' for each location, and each named vector holds the distance within
+#' `threshold` of the location.
+#' @param level string either "county" (default) or "zip"
+#' @param threshold numeric value; include in each location-specific named
+#'   vector only those locations that a within `threshold` distance units of the
+#'   target. The defaults is 50 (miles) and 15 (miles) for county and zip, respectively, but
+#'   these can be adjusted. Note if a different unit other than miles is used, then the
+#'   user should also adjust this parameter appropriately
+#' @param st string; optional to specify a state; if NULL distances are returned
+#'   for all zip codes or counties in the US
+#' @param unit string one of miles (default), kilometers, or meters; this is the
+#'   unit relevant to the threshold
+#' @export
+create_dist_list <- function(
+    level = c("county", "zip"),
+    threshold= {if(level=="county") 50 else 15},
+    st=NULL,
+    unit = c("miles", "kilometers", "meters")
+) {
+
+
+  state <- zip_code <- latitude <- longitude <- fips <- NULL
+
+  level = match.arg(level)
+  unit = match.arg(unit)
+  factor = list("miles" = 1609, "kilometers" = 1000, "meters"=1)[[unit]]
+
+  # In case threshold is null, revert to default
+  if(is.null(threshold)) {
+    threshold = ifelse(level=="county", 50, 15)
+  }
+
+  # convert within to meters
+  threshold = threshold*factor
+
+  if(level == "zip")
+    if(!is.null(st)) {
+      locs <- zipcodes[state == st, list(location = zip_code, latitude,longitude)]
+    } else {
+      locs <- zipcodes[, list(location = zip_code, latitude,longitude)]
+    }
+  else {
+    if(!is.null(st)) {
+      locs <- counties[state == st, list(location = fips, latitude,longitude)]
+    } else {
+      locs <- counties[, list(location = fips, latitude,longitude)]
+    }
+  }
+
+  # reduce to valid locations
+  locs <- locs[!is.na(latitude) & !latitude=="NULL"]
+
+  # replace any char with numeric
+  for(c in c("latitude", "longitude")) data.table::set(
+    locs, j=c, value=as.numeric(locs[[c]])
+  )
+
+  # convert to sf
+  locs <- sf::st_as_sf(locs, coords = c("longitude", "latitude"), crs=4236)
+
+  # get sparse predicate that holds just those locations within certain distance
+  # i.e. the neighbors
+  nb = sf::st_is_within_distance(locs, dist=threshold)
+
+  # get the number of locations
+  n <- nrow(locs)
+
+  # Flatten indices (aligned with sgbp order)
+  ii <- rep.int(seq_len(n), lengths(nb))
+  jj <- unlist(nb, use.names = FALSE)
+
+  # get the coordinates from locations
+  coords <- sf::st_coordinates(locs)
+
+  # Use geodist package to get the distances
+  d <- geodist::geodist(
+    x = coords[ii, , drop = FALSE],
+    y = coords[jj, , drop = FALSE],
+    paired = TRUE,
+    measure = "geodesic"
+  ) / factor
+
+  # convert to list
+  d <- split(d, rep.int(seq_len(n), lengths(nb)))
+
+  # Now, lets get the locations in each of d
+  loc_names <- locs$location[unlist(nb,use.names = F)]
+  loc_names <- split(loc_names, rep.int(seq_len(n), lengths(nb)))
+
+  d <- lapply(seq_along(d), \(i) sort(stats::setNames(d[[i]], loc_names[[i]])))
+  names(d) <- locs$location
+
+  return(d)
 }
